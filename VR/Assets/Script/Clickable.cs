8f182f5e90a04155dd7642a2d96eaa25e446fd2f
mvicky16 <vickyr.muhammad@gmail.com>
2019-09-20 01:06:59 +0700

    asset

---
+++ VR/Assets/Script/Clickable.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Clickable : MonoBehaviour {
+
+	private Rigidbody rb;
+    [SerializeField]
+    private float power = 100.0f;
+	
+	// Use this for initialization
+	void Start () {
+			rb = GetComponent<Rigidbody>();
+	}
+	
+	// Update is called once per frame
+	void FixedUpdate () {
+		float moveHorizontal = Input.GetAxis("Horizontal");
+        float moveVertical = Input.GetAxis("Vertical");
+
+        Vector3 movement = new Vector3(moveHorizontal, 0.0f, moveVertical);
+        rb.AddForce(movement*power);
+	}
+}
